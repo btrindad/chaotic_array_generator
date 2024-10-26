a56614d840d2fc7301866d7d950f421b0f018436
Bernardo <fudog931@gmail.com>
2024-10-26 14:14:39 -0400

    added handling for more flexible targets

--- pkg/array/generate_array.go
+++ pkg/array/generate_array.go
@@ -2,18 +2,25 @@ package array
 
 import (
 	"csci_6212/project3/test_cases/pkg/gen_sum"
+	"errors"
+	"fmt"
 	"math"
 	"math/rand"
 	"slices"
 )
 
-func GenerateArray(target int, length int) []int {
+func GenerateArray(target int, length int, min_value int) ([]int, error) {
+	err := check_args(target, length, min_value)
+	if err != nil {
+		return nil, err
+	}
+
 	left_length, right_length := split_length(length)
-	left_array := generate_sequence(target, left_length)
-	right_array := generate_sequence(target, right_length)
+	left_array := generate_sequence(target, left_length, 0)
+	right_array := generate_sequence(target, right_length, min_value)
 
 	output_array := join_arrays(left_array, right_array)
-	return output_array
+	return output_array, nil
 }
 
 // Divides a total length into two sub-lengths to generate
@@ -26,11 +33,20 @@ func split_length(length int) (int, int) {
 }
 
 // A wrapper for generating a particular sub sequence
-func generate_sequence(target int, length int) []int {
-	seq, err := gen_sum.GenerateSum(target, length)
+func generate_sequence(target int, length int, min_value int) []int {
+	shifted_target := (min_value * -1 * length) + target
+
+	seq, err := gen_sum.GenerateSum(shifted_target, length)
 	if err != nil {
 		// Crash if we hit an error
 		panic(err)
+	}
+
+	// This check isn't necessary, but potentially saves time
+	if min_value != 0 {
+		for i := 0; i < length; i++ {
+			seq[i] = seq[i] + min_value
+		}
 	}
 
 	return seq
@@ -46,4 +62,13 @@ func join_arrays(left []int, right []int) []int {
 	})
 
 	return output_array
+}
+
+func check_args(target int, length int, min_value int) error {
+	if length <= 0 {
+		msg := fmt.Sprintf("Invalid array length: %d", length)
+		return errors.New(msg)
+	}
+
+	return nil
 }
--- pkg/array/generate_array_test.go
+++ pkg/array/generate_array_test.go
@@ -7,28 +7,100 @@ import (
 
 func TestGenerateArray(t *testing.T) {
 	tests := map[string]struct {
-		target int
-		length int
+		target    int
+		length    int
+		min_value int
+		wantError bool
 	}{
 		"Simple Array": {
-			target: 30,
-			length: 50,
+			target:    30,
+			length:    50,
+			min_value: 0,
+			wantError: false,
+		},
+
+		"Array with negative value": {
+			target:    30,
+			length:    60,
+			min_value: -5,
+			wantError: false,
+		},
+
+		"Larger Array": {
+			target:    843,
+			length:    130,
+			min_value: -84,
+			wantError: false,
+		},
+
+		"Array with min value larger than target": {
+			target:    30,
+			length:    75,
+			min_value: -60,
+			wantError: false,
+		},
+
+		"Array with a positive min value": {
+			target:    30,
+			length:    75,
+			min_value: 10,
+			wantError: false,
+		},
+
+		"Array of 0": {
+			target:    30,
+			length:    0,
+			min_value: 0,
+			wantError: true,
+		},
+
+		"Negative Length": {
+			target:    10,
+			length:    -1,
+			min_value: 0,
+			wantError: true,
 		},
 	}
 
 	for msg, tc := range tests {
 		t.Run(msg, func(t *testing.T) {
-			arr := GenerateArray(tc.target, tc.length)
+			arr, err := GenerateArray(tc.target, tc.length, tc.min_value)
 
-			arr_sum := util.SumArray(arr)
-
-			if arr_sum != (2 * tc.target) {
-				t.Errorf("Array sum did not sum to the correct value. Expected: %d, Got: %d", (tc.target * 2), arr_sum)
+			if tc.wantError && err == nil {
+				t.Fatalf("Did not receive the error we expected")
 			}
 
-			if len(arr) != tc.length {
-				t.Errorf("Did not generate expected number of values. Expected: %d, Got: %d", tc.length, len(arr))
+			if !tc.wantError {
+				if err != nil {
+					t.Fatalf("Received unexpected error %s", err.Error())
+				}
+
+				arr_sum := util.SumArray(arr)
+
+				t.Logf("Debug Test <%s>: Sum => %d, Array => %v", msg, arr_sum, arr)
+
+				if arr_sum != (2 * tc.target) {
+					t.Errorf("Array sum did not sum to the correct value. Expected: %d, Got: %d", (tc.target * 2), arr_sum)
+				}
+
+				if len(arr) != tc.length {
+					t.Errorf("Did not generate expected number of values. Expected: %d, Got: %d", tc.length, len(arr))
+				}
+
+				if tc.min_value != 0 && !scan_array(arr, tc.min_value) {
+					t.Errorf("Array did not contain the requested minimum value %d, %v", tc.min_value, arr)
+				}
 			}
 		})
 	}
+}
+
+func scan_array(arr []int, target int) bool {
+	for _, elem := range arr {
+		if elem == target {
+			return true
+		}
+	}
+
+	return false
 }
